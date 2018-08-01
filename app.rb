9a18fd4987f0e3db69914b3ffebd433e5d8cdf4b
madeline57 <maddiejobeth@aol.com>
2018-08-01 16:49:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+get "/" do 
+  "hello world"
+end 
+get "/name" do 
+  erb :name
+end
+get "/hometown" do
+  erb :hometown
+end 
+get "/fav_song" do 
+  erb :fav_song
+end 
 end
